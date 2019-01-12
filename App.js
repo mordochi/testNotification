556554b7ab239226da94a20f4e84dc809158563a
mordochi <lilynnchi@gmail.com>
2019-01-12 17:59:19 +0800

    feat: install react-native-push-notification for ios and test local notification

--- App.js
+++ App.js
@@ -7,7 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Switch, AppState } from 'react-native';
+import PushNotification from 'react-native-push-notification';
+import PushController from './PushController.js';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +21,50 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notification: true,
+    }
+
+    this.switchNotification = this.switchNotification.bind(this);
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  }
+
+  handleAppStateChange(appState) {
+    if(appState === 'inactive') {
+      console.log(appState)
+      PushNotification.localNotificationSchedule({
+        //... You can use all the options from localNotifications
+        message: appState, // (required)
+        date: new Date(Date.now() + (1 * 1000)) // in 60 secs
+      });
+    }
+  }
+
+  switchNotification() {
+    this.setState({
+      notification: !this.state.notification,
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Switch value={this.state.notification} onValueChange={() => this.switchNotification()} />
+        <PushController />
       </View>
     );
   }
